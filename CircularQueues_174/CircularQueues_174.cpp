ef2e317e8d8c4c065a034dd89e04e7ba091fa24e
adandoang <akhdanjzif@gmail.com>
2023-06-23 20:22:11 +0700

    void remove

--- CircularQueues_174/CircularQueues_174.cpp
+++ CircularQueues_174/CircularQueues_174.cpp
@@ -37,4 +37,12 @@ public:
 		}
 		queue_array[REAR] = num;
 	}
+
+	void remove() {
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "queue underflow\n";
+			return;
+		}
+		cout << "\nThe element delete from the queue is: " << queue_array[FRONT] << "\n";
 };
