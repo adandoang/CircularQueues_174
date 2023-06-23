a40937637798bc4e3fab44f01d39fb901eb2cf58
adandoang <akhdanjzif@gmail.com>
2023-06-23 20:39:31 +0700

    int main while

--- CircularQueues_174/CircularQueues_174.cpp
+++ CircularQueues_174/CircularQueues_174.cpp
@@ -90,3 +90,18 @@ public:
 		}
 	}
 };
+
+int main() {
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement Insert Operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+		}
+	}
+}
